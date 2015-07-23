c32e0d8b9e9a1efb3b2746161aab5f7a2ae89a10
Richard Good <rrg@richards-imac.local>
2015-07-23 13:15:02 -0700

    Initial Commit

---
+++ HSCoredata/HSCoredata.h
@@ -0,0 +1,19 @@
+//
+//  HSCoredata.h
+//  HSCoredata
+//
+//  Created by Richard Good on 7/23/15.
+//  Copyright © 2015 HannibalStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HSCoredata.
+FOUNDATION_EXPORT double HSCoredataVersionNumber;
+
+//! Project version string for HSCoredata.
+FOUNDATION_EXPORT const unsigned char HSCoredataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HSCoredata/PublicHeader.h>
+
+
